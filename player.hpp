a0cdafec7286bab27822443fa4afd67dfd852f4f
Nathaniel Smith <nismith@caltech.edu>
2018-03-06 12:38:28 -0800

    Readded player.hpp (it seems to be missing from the official repository.

---
+++ player.hpp
@@ -0,0 +1,41 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <iostream>
+#include "common.hpp"
+#include "board.hpp"
+using namespace std;
+
+class Player {
+
+public:
+    Player(Side side);
+    ~Player();
+
+    Move *doMove(Move *opponentsMove, int msLeft);
+    Move *doSimpleMove(Move *opponentsMove, int msLeft);
+    Move *doCornerMove(Move *opponentsMove, int msLeft);
+    Move *doGreedyMove(Move *opponentsMove, int msLeft);
+
+    
+    double depth2_eval(Board *board);
+    
+    double depth2_minimax(Board *board, int depth, bool isMax, Side side,
+                          int msLeft, int limit);
+    Move* minimax_move(Board *board, int depth, bool isMax, Side side, 
+                          int msLeft, int limit);
+    
+    
+    double evaluate(Board *board);
+    double cat_eval(double max_val, double min_val);
+    
+    // Flag to tell if the player is running within the test_minimax context
+    bool testingMinimax;
+
+private:
+	Side mySide;
+	Side oppSide;
+	Board *board;
+};
+
+#endif
